183a2b7e285988d43105dff8e8b6f4603020605f
Bartosz Dziewoński <dziewonski@fastmail.fm>
2025-09-23 05:40:45 +0200

    Only parse and process the HTML if it seems to contain our tags (for topic subscriptions)
    Follow-up to b6324d643d7a53da77bee33fd0dff4afa93b34e3.
    CommentFormatter::postprocessTopicSubscription() has its own
    HTML parsing pass, and needs a separate check.
    
    Bug: T400115
    Change-Id: I560e985a195a50099c5b4e7f89a2971c2604a648

--- includes/CommentFormatter.php
+++ includes/CommentFormatter.php
@@ -486,6 +486,11 @@ class CommentFormatter {
 		string $text, BatchModifyElements &$batchModifyElements, IContextSource $contextSource,
 		SubscriptionStore $subscriptionStore, bool $isMobile, bool $useButtons
 	): void {
+		// Optimization: Only parse and process the HTML if it seems to contain our tags (T400115)
+		if ( !str_contains( $text, '<mw:dt-subscribebutton' ) && !str_contains( $text, '<dt-subscribebutton' ) ) {
+			return;
+		}
+
 		$doc = DOMCompat::newDocument( true );
 
 		$itemDataByName = [];
